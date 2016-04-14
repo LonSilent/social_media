0f47108f8b90b2072242787b168c838a416dcc2c
LonSilent <101703044@nccu.edu.tw>
2016-04-14 19:25:54 +0800

    idf

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 import csv
 import jieba
+import math
+import re
 from collections import Counter
 
 with open('csquare_all.csv', 'r') as f:
@@ -20,6 +22,11 @@ with open('csquare_all.csv', 'r') as f:
 i = 1
 seg_list = []
 cut_list = []
+df_dic = {}
+df_list = []
+# df_dic["aa"] = 1
+# df_dic["aa"] += 1
+# print(df_dic)
 
 symbol_list = ["\""," ","，","/","#","-","↓","！",".",")","「","」","】","【","？","(",
 	":","✨","、","★","。","：","!","）","~",";","_","▷","◇","\'","　","●","?","＿",
@@ -27,24 +34,60 @@ symbol_list = ["\""," ","，","/","#","-","↓","！",".",")","「","」","】",
 
 while(i<len(file_list)):
 	for symbol in symbol_list:
-		file_list[i][7] = file_list[i][7].replace(symbol,"") 
+		file_list[i][7] = file_list[i][7].replace(symbol,"")
+	temp = list(jieba.cut(file_list[i][7], cut_all=False))
+	for term in temp:
+		if(term in df_list):
+			pass
+		else:
+			if(term in df_dic):
+				df_dic[term] += 1
+			else:
+				df_dic[term] = 1
+			df_list.append(term)
+	del df_list[:]
+	del temp[:]
+	i+=1
+
+# i = (int)(0.75*len(file_list))
+i=1
+
+while(i<len(file_list)):
+	for symbol in symbol_list:
+		file_list[i][7] = file_list[i][7].replace(symbol,"")
 	temp = list(jieba.cut(file_list[i][7], cut_all=False))
 	seg_list += temp
 	del temp[:]
 	i+=1
 
+# print(df_dic)
+
 result = Counter(seg_list)
 
 l = []
 for key,value in result.items():
-	temp = [key,value]
+	temp = [key, 0.1 + math.log10(value)]
 	l.append(temp)
 l.sort(key=lambda x: x[1])
+# l = l[::-1]
+# print(l)
+
+i = 0
+
+while(i<len(l)):
+	idf = math.log10(1+len(file_list)/df_dic[l[i][0]])
+	# print(idf)
+	tfidf = l[i][1]*idf
+	l[i].append(tfidf)
+	i+=1
+
+# print(l)
+l.sort(key=lambda x: x[2])
 l = l[::-1]
 # print(l)
 
 i = 0
 with open("output_csquare_all.csv", "w") as f:
 	while(i<len(l)):
-		f.write(l[i][0]+','+str(l[i][1])+'\n')
+		f.write(l[i][0]+','+str(l[i][1])+','+str(l[i][2])+'\n')
 		i+=1
