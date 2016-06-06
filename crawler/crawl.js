22574e824caeb76f6a392e39db0bd59df61561eb
LonSilent <101703044@nccu.edu.tw>
2016-06-06 21:14:29 +0800

    full crawler

--- crawler/crawl.js
+++ crawler/crawl.js
@@ -1,21 +1,21 @@
 var request = require('request');
 var fs = require('fs');
 var _ = require('lodash');
-var beauty = require('./data/japan.json');
+var beauty = require('./data/food.json');
 
-food = _.uniq(beauty, "number");
+var food = _.uniq(beauty, "number");
 
 // console.log(food[1]);
 
-var file_name = 'result_japan.csv';
+var file_name = 'result_food.csv';
 
-const offset = 0;
-const limit = food.length-1;
+const offset = 10800;
+const limit = offset + 600;
 
 
-fs.writeFile(file_name, 'title,url,hits,user,device,hour,count_images,count_comments,' + 'body\n', function(err) {
-	if (err) return console.log(err);
-});
+// fs.writeFile(file_name, 'title,url,hits,user,device,hour,search_words,count_images,count_comments,' + 'body\n', function(err) {
+// 	if (err) return console.log(err);
+// });
 
 getter(offset);
 
@@ -30,7 +30,7 @@ function RemoveHTML(strText) {
 function getter(i) {
 	if (i < limit) {
 		var url = 'https://emma.pixnet.cc/blog/articles/' + food[i].number + '?format=json&user=' + food[i].id +
-			'&pretty_print=1?client_id={890707c454da63b6d54b45c6b067ec8a}';
+			'&client_id=890707c454da63b6d54b45c6b067ec8a';
 		request(url, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				result = JSON.parse(body);
@@ -43,17 +43,22 @@ function getter(i) {
 					result.article.user.name + ',' +
 					food[i].device + ',' +
 					food[i].hour + ',' +
+					food[i].words + ',' +
 					result.article.images.length + ',' +
 					result.article.info.comments_count + ',' + body + '\n';
 				console.log("success: " + i);
 				fs.appendFile(file_name, string, function(err) {
 					if (err) return console.log(err);
 				});
-				getter(i + 1)
+				setTimeout(function() {
+					getter(i + 1);
+				}, 100);
 			} else {
 				console.log(error);
 				console.log("error: " + i);
-				getter(i + 1)
+				setTimeout(function() {
+					getter(i + 1);
+				}, 100);
 			}
 		});
 	}
